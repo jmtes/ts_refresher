74ba257e9c161e3047d4b3f152780acb46b2de3f
juno tesoro <38450133+jmtes@users.noreply.github.com>
2023-09-30 22:22:12 +0000

    Do the golden code project

--- projects/functions/secret-secrets/03-the-golden-code/index.ts
+++ projects/functions/secret-secrets/03-the-golden-code/index.ts
@@ -1,2 +1,22 @@
 // Write your createCodeCracker function here! ✨
 // You'll need to export it so the tests can run it.
+
+type Settings = {
+	attempts: number;
+	makeGuess: (text: string, attempt: number) => string;
+	validateGuess: (guess: string) => boolean;
+};
+
+export function createCodeCracker({
+	attempts,
+	makeGuess,
+	validateGuess,
+}: Settings): (text: string) => string | undefined {
+	return (text) => {
+		for (let i = 0; i < attempts; i++) {
+			const guess = makeGuess(text, i);
+
+			if (validateGuess(guess)) return guess;
+		}
+	};
+}
