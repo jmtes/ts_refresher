4cbb39e6eb7c37551eaf480aff47b06305944cde
juno tesoro <38450133+jmtes@users.noreply.github.com>
2023-09-30 21:42:19 +0000

    Do incoming cipher project

--- projects/functions/secret-secrets/01-incoming-cipher/index.ts
+++ projects/functions/secret-secrets/01-incoming-cipher/index.ts
@@ -1,2 +1,16 @@
 // Write your createCipher function here! ✨
 // You'll need to export it so the tests can run it.
+
+type StringToString = (arg: string) => string;
+
+export function createCipher(cipher: StringToString): StringToString {
+	return (text: string) => {
+		let str = "";
+
+		for (const char of text) {
+			str = str.concat(cipher(char));
+		}
+
+		return str;
+	};
+}
